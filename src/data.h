fe16ac9048d390b5b658c02310c23161e04852a8
w.y.f <w.y.f>
2017-06-02 06:21:35 +0800

    v.10

--- src/data.h
+++ src/data.h
@@ -6,14 +6,34 @@
 #include <map>
 class usePoly{
 public:
-    // void addPolynomial(const std::string& name, const std::string & poly);
     bool findPolynomial(const std::string& name) const;
+    //在多项式库中寻找有无该标识符对应的多项式
     void addPolynomial(const std::string& name, const Polynomial & poly);
+    //在多项式库中添加该标识符对应的多项式
     void deletePolynomial(const std::string&);
+    //在多项式库中删除该标识符对应的多项式
     Polynomial getPolynomial(const std::string&);
+    //在该多项式库中取得该标识符对应的多项式
     void printAll();
+    //格式化打印该多项式库中存有的所有多项式
 private:
     std::map<std::string, Polynomial> data; 
+    //索引是多项式对应的标识符，内容是该多项式
 };
 
 #endif
+
+
+// bool findPolynomial(const std::string& name) const;
+// //在多项式库中寻找有无该标识符对应的多项式
+// void addPolynomial(const std::string& name, const Polynomial & poly);
+// //在多项式库中添加该标识符对应的多项式
+// void deletePolynomial(const std::string&);
+// //在多项式库中删除该标识符对应的多项式
+// Polynomial getPolynomial(const std::string&);
+// //在该多项式库中取得该标识符对应的多项式
+// void printAll();
+// //格式化打印该多项式库中存有的所有多项式
+
+// std::map<std::string, Polynomial> data; 
+//索引是多项式对应的标识符，内容是该多项式
--- src/polynomial.h
+++ src/polynomial.h
@@ -21,29 +21,32 @@ public:
         bool operator!=(const item& rhs) const{return ((power != rhs.power)||(coefficient != rhs.coefficient));};
     };// 每一项
 
+//构造函数
     Polynomial(int maxPower = MAX_ITEM_NUM);
     // 生成（maxPower+1）项的多项式，并且初始化指数为对应数字，系数为0
-
     Polynomial(const std::vector<item> t);
     // 使用vector来初始化多项式的系数
     // 注意此vector的内容需合法
 
-    // explicit Polynomial(const std::string & poly);// 必须用合法的字符串才能初始化
 
-// overload operator
+//重载运算符
     Polynomial operator+(const Polynomial& rhs) const;//多项式相加
     Polynomial operator-(const Polynomial& rhs) const;//多项式相减
     Polynomial operator*(const item& rhs) const;//多项式乘某一项
     Polynomial operator*(const Polynomial& rhs) const;//多项式乘多项式
     Polynomial operator*(coefficient_t rhs) const;//多项式的数乘
-    friend Polynomial operator*(coefficient_t lhs, const Polynomial& rhs);//多项式的数乘
+    friend Polynomial operator*(coefficient_t lhs, const Polynomial& rhs);
+        //多项式的数乘
     bool operator==(const Polynomial& rhs) const;
+
+// 运算接口
     double evaluate(coefficient_t arg) const;// 多项式求值
-    Polynomial derivative() const;
+    Polynomial derivative() const; //多项式求导
 
 // I/O
     std::string toString() const;//多项式转变成字符串类型输出
-    friend std::ostream & operator<<(std::ostream& out, const Polynomial & rhs);//使用cout输出
+    friend std::ostream & operator<<(std::ostream& out, const Polynomial & rhs);
+    //使用cout输出
 
 private:
     power_t _maxPower; // 最大次数, +1 即为数组存放的项数
@@ -52,3 +55,19 @@ private:
 
 
 #endif
+
+
+
+// // 数据成员如下
+// power_t _maxPower; // 最大次数, +1 即为数组存放的项数
+// std::vector <item> _items;// 存放每一项的数组，该数组默认最大项数为MAX_ITEM_NUM 为50
+// // 其中
+// typedef double coefficient_t;//系数类型为double
+// typedef int power_t;//指数类型为int
+// struct item{
+//     power_t power;//每一项的次数
+//     coefficient_t coefficient;//系数
+//     item(power_t arg, coefficient_t arg2):power(arg),coefficient(arg2) {}
+//     bool operator<(const item& rhs) const;
+//     bool operator!=(const item& rhs) const;
+// };// 每一项
