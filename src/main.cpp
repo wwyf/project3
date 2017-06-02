71eebde25cba23901754ecf6e9caf5eb4206c54e
WalkerYF <Walkeryf@yeah.net>
2017-06-02 14:52:18 +0800

    finished

--- src/main.cpp
+++ src/main.cpp
@@ -207,10 +207,3 @@ int main(){
 }
 
 
-case '2' :{
-    Polynomial myPoly1 = Input("please write down your first polynomial.\n");
-    Polynomial myPoly2 = Input("please write down your second polynomial.\n");
-    cout << "(" << myPoly1 << ")" << "+" << "(" << myPoly2 << ")"<< "=";
-    Output(myPoly1 + myPoly2,"");
-    break;
-}
